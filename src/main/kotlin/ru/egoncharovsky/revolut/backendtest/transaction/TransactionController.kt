75867ab22c428ddd509e9a31550c0d0b471345e3
Evgeny Goncharovsky <egoncharovsky@gmail.com>
2019-09-01 13:25:32 +0300

    transfer money operation

--- src/main/kotlin/ru/egoncharovsky/revolut/backendtest/transaction/TransactionController.kt
+++ src/main/kotlin/ru/egoncharovsky/revolut/backendtest/transaction/TransactionController.kt
@@ -1,14 +1,25 @@
 package ru.egoncharovsky.revolut.backendtest.transaction
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
 
 @RestController
-@RequestMapping("/backend-test")
-class TransactionController {
+@RequestMapping("/transaction")
+class TransactionController(
+        @Autowired private val transactionService: TransactionService
+) {
 
-    @PostMapping("/test")
-    fun test() = "Test successful!"
+    @PostMapping("/transfer-money")
+    fun transferMoney(transfer: TransferMoneyDto) {
+        transactionService.transferMoney(transfer.fromAccountId, transfer.toAccountId, transfer.amount)
+    }
 
+    data class TransferMoneyDto(
+            val fromAccountId: Long,
+            val toAccountId: Long,
+            val amount: BigDecimal
+    )
 }
